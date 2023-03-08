e150b1776f3fc8ae6731357e410b283eb6fa5686
REAL-ERP <124227412+REAL-ERP@users.noreply.github.com>
2023-03-08 16:33:22 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="okcoin-GBRUNNER", # Replace with your own username
-    version="0.0.3",
-    author="Gregory Brunner",
-    author_email="gregory.brunner@protonmail.com",
-    description="A Python package for interfacing with okcoin.",
+    name="okcoinv5", # Replace with your own username
+    version="0.0.1",
+    author="EP-FTW",
+    author_email="info@milightningrod.com",
+    description="A Python package for interfacing with okcoin API V5, which includes Lightning functionality.",
     long_description="Longer description here.",
     long_description_content_type="text/markdown",
-    url="https://github.com/gbrunner/python-api-for-okcoin",
+    url="https://github.com/REAL-ERP/python-api-for-okcoin-V5/",
     project_urls={
-        "Bug Tracker": "https://github.com/gbrunner/python-api-for-okcoin/issues",
+        "Bug Tracker": "https://github.com/REAL-ERP/python-api-for-okcoin-V5/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -22,5 +22,5 @@ setuptools.setup(
     ],
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.6",
+    python_requires=">=3.10",
 )
